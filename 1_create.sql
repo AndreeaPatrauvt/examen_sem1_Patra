62d2c491654177f3f57e427026b4fe3869720627
AndreeaPatrauvt <andreea.patra03@e-uvt.ro>
2025-02-06 22:26:32 +0200

    final_create.sql

--- 1_create.sql
+++ 1_create.sql
@@ -41,4 +41,64 @@ suprafata decimal null,
 sala_fitness_id integer not null references sala_fitness(id)
 );
 
+create table if not exists tip_abonament 
+(
+id serial primary key,
+tip_abonament varchar (100) not null
+);
 
+create table if not exists abonament
+(
+id serial primary key,
+tarif numeric not null,
+durata_luni smallint not null,
+sala_fitness_id integer not null references sala_fitness(id),
+id_tip_abonament integer not null references tip_abonament(id)
+);
+
+create table if not exists membri
+(
+id serial primary key,
+cod_membru varchar(10) not null unique,
+data_inscriere date not null,
+data_expirare date null
+);
+
+create table if not exists persoane 
+(
+id serial primary key,
+nume varchar(100) not null,
+numar_telefon varchar (15) not null unique,
+email varchar (100),
+adresa text null,
+membri_id integer not null unique references membri(id)
+);
+
+create table if not exists abonament_membru
+(
+id serial primary key,
+membri_id integer not null references membri(id),
+tip_abonament_id integer not null references tip_abonament(id)
+);
+
+create table if not exists antrenori
+(
+id serial primary key, 
+nume varchar(100) not null, 
+numar_telefon varchar (15) null,
+nota_review numeric null,
+sala_fitness_id integer not null references sala_fitness(id)
+);
+
+create table if not exists specializare
+(
+id serial primary key,
+nume_specializare varchar (100) not null
+);
+
+create table if not exists antrenori_specializare
+(
+id serial primary key,
+specializare_id integer not null references specializare(id),
+antrenori_id integer not null references antrenori(id)
+);
