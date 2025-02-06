5d34f363cf3798ab84e1d1f730af182856d9dfe1
AndreeaPatrauvt <andreea.patra03@e-uvt.ro>
2025-02-07 00:58:19 +0200

    final_update.sql

---
+++ 3_update.sql
@@ -0,0 +1,133 @@
+update tara set nume='romania' where fid=1;
+update tara set nume='france' where fid=12;
+update tara set nume='germany' where fid=13;
+update tara set nume='italy' where fid=14;
+update tara set nume='spain' where fid=15;
+update tara set nume='england' where fid=16;
+update tara set nume='hungary' where fid=17;
+update tara set nume='bulgary' where fid=18;
+update tara set nume='belgium' where fid=19;
+update tara set nume='greece' where fid=20;
+
+UPDATE judet SET nume = 'timiș' WHERE fid = 1;
+UPDATE judet SET nume = 'arad' WHERE fid = 2;
+UPDATE judet SET nume = 'hunedoara' WHERE fid = 3;
+UPDATE judet SET nume = 'maramureș' WHERE fid = 4;
+UPDATE judet SET nume = 'mehedinți' WHERE fid = 5;
+UPDATE judet SET nume = 'constanța' WHERE fid = 6;
+UPDATE judet SET nume = 'cluj' WHERE fid = 7;
+UPDATE judet SET nume = 'alba' WHERE fid = 8;
+UPDATE judet SET nume = 'bucurești' WHERE fid = 9;
+UPDATE judet SET nume = 'mureș' WHERE fid = 10;
+
+UPDATE uat SET nume = 'timișoara' WHERE fid = 8;
+UPDATE uat SET nume = 'lugoj' WHERE fid = 9;
+UPDATE uat SET nume = 'arad' WHERE fid = 10;
+UPDATE uat SET nume = 'lipova' WHERE fid = 11;
+UPDATE uat SET nume = 'hunedoara' WHERE fid = 12;
+UPDATE uat SET nume = 'deva' WHERE fid = 13;
+UPDATE uat SET nume = 'simeria' WHERE fid = 14;
+UPDATE uat SET nume = 'drobeta' WHERE fid = 15;
+UPDATE uat SET nume = 'bucurești' WHERE fid = 16;
+UPDATE uat SET nume = 'cluj' WHERE fid = 17;
+
+UPDATE sala_fitness SET nume = 'Gym 1' WHERE id = 1;
+UPDATE sala_fitness SET nume = 'Gym 2' WHERE id = 2;
+UPDATE sala_fitness SET nume = 'Gym 3' WHERE id = 3;
+UPDATE sala_fitness SET nume = 'Gym 4' WHERE id = 4;
+UPDATE sala_fitness SET nume = 'Gym 5' WHERE id = 5;
+UPDATE sala_fitness SET nume = 'Gym 6' WHERE id = 6;
+UPDATE sala_fitness SET nume = 'Gym 7' WHERE id = 7;
+UPDATE sala_fitness SET nume = 'Gym 8' WHERE id = 8;
+UPDATE sala_fitness SET nume = 'Gym 9' WHERE id = 9;
+UPDATE sala_fitness SET nume = 'Gym 10' WHERE id = 10;
+
+update sali_antrenament set numar_sali = 5 where id =1;
+UPDATE sali_antrenament SET numar_sali = 3 WHERE id = 2;
+UPDATE sali_antrenament SET numar_sali = 4 WHERE id = 3;
+UPDATE sali_antrenament SET numar_sali = 2 WHERE id = 4;
+UPDATE sali_antrenament SET numar_sali = 2 WHERE id = 5;
+UPDATE sali_antrenament SET numar_sali = 3 WHERE id = 6;
+UPDATE sali_antrenament SET numar_sali = 2 WHERE id = 7;
+UPDATE sali_antrenament SET numar_sali = 3 WHERE id = 8;
+UPDATE sali_antrenament SET numar_sali = 5 WHERE id = 9;
+UPDATE sali_antrenament SET numar_sali = 6 WHERE id = 10;
+UPDATE sali_antrenament SET numar_sali = 2 WHERE id = 11;
+
+UPDATE tip_abonament SET tip_abonament = 'morning boost' WHERE id = 1;
+UPDATE tip_abonament SET tip_abonament = 'kids fun fitness' WHERE id = 2;
+UPDATE tip_abonament SET tip_abonament = 'pilates & stretch' WHERE id = 3;
+UPDATE tip_abonament SET tip_abonament = 'unlimited gym access' WHERE id = 4;
+UPDATE tip_abonament SET tip_abonament = 'cardio & strength' WHERE id = 5;
+UPDATE tip_abonament SET tip_abonament = 'cycling endurance' WHERE id = 6;
+UPDATE tip_abonament SET tip_abonament = 'mind & body yoga' WHERE id = 7;
+UPDATE tip_abonament SET tip_abonament = 'muscle gain pro' WHERE id = 8;
+UPDATE tip_abonament SET tip_abonament = 'relaxation sauna' WHERE id = 9;
+UPDATE tip_abonament SET tip_abonament =  'massage' WHERE id = 10;
+
+UPDATE abonament SET tarif = 120 WHERE id = 10;
+UPDATE abonament SET tarif = 220 WHERE id = 11;
+UPDATE abonament SET tarif = 320 WHERE id = 12;
+UPDATE abonament SET tarif = 1100 WHERE id = 13;
+UPDATE abonament SET tarif = 550 WHERE id = 14;
+UPDATE abonament SET tarif = 450 WHERE id = 15;
+UPDATE abonament SET tarif = 350 WHERE id = 16;
+UPDATE abonament SET tarif = 650 WHERE id = 17;
+UPDATE abonament SET tarif = 520 WHERE id = 18;
+UPDATE abonament SET tarif = 250 WHERE id = 19;
+
+UPDATE membri SET cod_membru = 'as310' WHERE id = 1;
+UPDATE membri SET cod_membru = 'am410' WHERE id = 10;
+UPDATE membri SET cod_membru = 'an510' WHERE id = 11;
+UPDATE membri SET cod_membru = 'nb610' WHERE id = 12;
+UPDATE membri SET cod_membru = 'ar510' WHERE id = 13;
+UPDATE membri SET cod_membru = 'fn810' WHERE id = 14;
+UPDATE membri SET cod_membru = 'dm910' WHERE id = 15;
+UPDATE membri SET cod_membru = 'as710' WHERE id = 16;
+UPDATE membri SET cod_membru = 'lm410' WHERE id = 17;
+UPDATE membri SET cod_membru = 'rd510' WHERE id = 18;
+
+UPDATE persoane SET nume = 'Adrian Popescu' WHERE id = 2;
+UPDATE persoane SET nume = 'Andrei Ionescu' WHERE id = 3;
+UPDATE persoane SET nume = 'Mihai Popa' WHERE id = 4;
+UPDATE persoane SET nume = 'Ioana Radu' WHERE id = 5;
+UPDATE persoane SET nume = 'Andreea Dumitru' WHERE id = 6;
+UPDATE persoane SET nume = 'Valentin Georgescu' WHERE id = 7;
+UPDATE persoane SET nume = 'Marius Ene' WHERE id = 8;
+UPDATE persoane SET nume = 'Mihai Stan' WHERE id = 9;
+UPDATE persoane SET nume = 'Roxana Ilie' WHERE id = 10;
+UPDATE persoane SET nume = 'Mihaela Tudor' WHERE id = 11;
+
+UPDATE antrenori SET nota_review = 4.5 WHERE id = 1;
+UPDATE antrenori SET nota_review = 4.8 WHERE id = 2;
+UPDATE antrenori SET nota_review = 4.2 WHERE id = 3;
+UPDATE antrenori SET nota_review = 4.7 WHERE id = 4;
+UPDATE antrenori SET nota_review = 4.3 WHERE id = 5;
+UPDATE antrenori SET nota_review = 4.6 WHERE id = 6;
+UPDATE antrenori SET nota_review = 4.1 WHERE id = 7;
+UPDATE antrenori SET nota_review = 4.9 WHERE id = 8;
+UPDATE antrenori SET nota_review = 4.4 WHERE id = 9;
+UPDATE antrenori SET nota_review = 4.7 WHERE id = 10;
+
+UPDATE specializare SET nume_specializare = 'yoga & mindfulness' WHERE id = 1;
+UPDATE specializare SET nume_specializare = 'advanced bodybuilding' WHERE id = 2;
+UPDATE specializare SET nume_specializare = 'swimming techniques' WHERE id = 3;
+UPDATE specializare SET nume_specializare = 'functional fitness' WHERE id = 4;
+UPDATE specializare SET nume_specializare = 'pilates & core training' WHERE id = 5;
+UPDATE specializare SET nume_specializare = 'personal training & nutrition' WHERE id = 6;
+UPDATE specializare SET nume_specializare = 'high-intensity aerobic' WHERE id = 7;
+UPDATE specializare SET nume_specializare = 'flexibility & stretching' WHERE id = 8;
+UPDATE specializare SET nume_specializare = 'dance & performance' WHERE id = 9;
+UPDATE specializare SET nume_specializare = 'zumba & cardio dance' WHERE id = 10;
+
+UPDATE antrenori_specializare SET specializare_id = 4 WHERE id = 1;
+UPDATE antrenori_specializare SET specializare_id = 6 WHERE id = 2;
+UPDATE antrenori_specializare SET specializare_id = 2 WHERE id = 3;
+UPDATE antrenori_specializare SET specializare_id = 7 WHERE id = 4;
+UPDATE antrenori_specializare SET specializare_id = 9 WHERE id = 5;
+UPDATE antrenori_specializare SET specializare_id = 3 WHERE id = 6;
+UPDATE antrenori_specializare SET specializare_id = 10 WHERE id = 7;
+UPDATE antrenori_specializare SET specializare_id = 1 WHERE id = 8;
+UPDATE antrenori_specializare SET specializare_id = 5 WHERE id = 9;
+UPDATE antrenori_specializare SET specializare_id = 8 WHERE id = 10;
+
